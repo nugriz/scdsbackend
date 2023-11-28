2d44beceae9a2b075eb9276cdc256fb44bc74c3d
nugriz <rizki@happy5.co>
2023-11-28 11:03:27 +0700

    allow cors

--- actions/app.go
+++ actions/app.go
@@ -48,7 +48,7 @@ func App() *buffalo.App {
 			Env:          ENV,
 			SessionStore: sessions.Null{},
 			PreWares: []buffalo.PreWare{
-				cors.Default().Handler,
+				cors.AllowAll().Handler,
 			},
 			SessionName: "_scdsbackend_session",
 		})
